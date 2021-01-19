1c391783238078ff0bd7d7b4d960424139d4f5ba
Thomas Mao <tm1813@gmail.com>
2021-01-19 14:03:44 +0800

    add sample files

---
+++ demoGroovy/src/main/groovy/com/example/demoGroovy/DemoGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.demoGroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoGroovyApplication, args)
+	}
+
+}
---
+++ demoGroovy/src/test/groovy/com/example/demoGroovy/DemoGroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.demoGroovy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
