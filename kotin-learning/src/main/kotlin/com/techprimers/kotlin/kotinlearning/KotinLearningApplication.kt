fca4f7a2b11566ee888aa58e73ba9355079459ad
Thomas Mao <tm1813@gmail.com>
2021-01-20 14:31:01 +0800

    kotlin learning

---
+++ kotin-learning/src/main/kotlin/com/techprimers/kotlin/kotinlearning/KotinLearningApplication.kt
@@ -0,0 +1,11 @@
+package com.techprimers.kotlin.kotinlearning
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotinLearningApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotinLearningApplication>(*args)
+}
---
+++ kotin-learning/src/test/kotlin/com/techprimers/kotlin/kotinlearning/KotinLearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.techprimers.kotlin.kotinlearning
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotinLearningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
